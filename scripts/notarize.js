d829e7420c9905c5c77032d8e6a7753d9558b8fc
nalbam <me@nalbam.com>
2025-04-19 14:35:38 +0900

    Update version in package.json from v0.0.4 to v0.0.5 and modify notarization script to include default values for appleId, appleIdPassword, and teamId.

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,13 +1,17 @@
-const { notarize } = require('electron-notarize');
-
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
 
   if (electronPlatformName !== 'darwin') {
+    console.log('Skipping notarization: Not macOS build.');
     return;
   }
 
+  // 여기 안에서만 electron-notarize를 require 합니다
+  const { notarize } = require('electron-notarize');
+
   const appName = context.packager.appInfo.productFilename;
+
+  console.log(`Starting notarization for app: ${appName}`);
 
   return await notarize({
     appBundleId: 'com.opspresso.toast-app',
